937dfb01e8f77a99ef919490db4a42f17250c5d3
urba <patryk.urba@nokia.com>
2022-12-22 09:47:52 +0100

    refactor2

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -21,7 +21,7 @@ void GildedRose::updateQuality()
         {
             if (item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros")
             {
-                item.quality = item.quality - 1;
+                --item.quality;
             }
         }
         else if (item.quality < 50)
@@ -32,38 +32,40 @@ void GildedRose::updateQuality()
             {
                 if (item.sellIn < 11 and item.quality < 50)
                 {
-                        item.quality = item.quality + 1;
+                    ++item.quality;
                 }
 
                 if (item.sellIn < 6 and item.quality < 50)
                 {
-                    item.quality = item.quality + 1;
+                    ++item.quality;
                 }
             }
         }
 
         if (item.name != "Sulfuras, Hand of Ragnaros")
         {
-            item.sellIn = item.sellIn - 1;
+            --item.sellIn;
         }
 
         if (item.sellIn < 0)
         {
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                item.quality = 0;
+            }
+
             if (item.name == "Aged Brie")
             {
                 if (item.quality < 50)
                     {
-                        item.quality = item.quality + 1;
+                        ++item.quality;
                     }
 
             }
-            else if (item.name == "Backstage passes to a TAFKAL80ETC concert")
-            {
-                item.quality = item.quality - item.quality;
-            }
+
             else if (item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros")
             {
-                item.quality = item.quality - 1;
+                --item.quality;
             }
         }
     }
